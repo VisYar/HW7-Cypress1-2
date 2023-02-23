2d2cb6ab9f8857972a9b8f288a00cb0d8636c432
Tatyana Dmitrieva <visyar76@gmail.com>
2023-02-23 19:52:38 +0300

    Changed file bookingMovieTest

--- cypress/e2e/bookingMovieTest.cy.js
+++ cypress/e2e/bookingMovieTest.cy.js
@@ -13,7 +13,7 @@ describe("Booking a movie", () => {
     cy.get(selector.sessionGrid).eq(5).then(($title) => {
     	cy.visit("/client");
     	cy.get(selector.dayOfWeek).click();
-    	cy.get(selector.movieSection).contains($title.text()).parent().parent().next().click(30,40);
+    	cy.get(selector.movieSection).contains($title.text()).parent().parent().next().contains("12:00").click();
     	cy.choosePlace(8, 6, 7);
     	cy.get(selector.bookButton).click();
     	cy.contains("Вы выбрали билеты:").should("be.visible");
